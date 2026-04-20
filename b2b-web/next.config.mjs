c255438947f53267e05a86012bab5f207a629ead
germanrobles-pa <german@pavimentosarquiservi.com>
2026-04-20 12:37:49 +0200

    Ignorar lint en build para deploy inicial

--- b2b-web/next.config.mjs
+++ b2b-web/next.config.mjs
@@ -1,13 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // ESTO PERMITE QUE EL BUILD TERMINE AUNQUE HAYA ERRORES DE TIPO
     ignoreBuildErrors: true
   },
   eslint: {
-    // ESTO IGNORA LOS ERRORES DE LINTING
     ignoreDuringBuilds: true
-  }
+  },
+  distDir: ".next"
 };
 
 export default nextConfig;
